ce31027998c7ee20e212677db29b69610eba1d91
Naka <tomas.nakasone@arreglatechcompany.com>
2025-02-03 19:42:06 -0300

    Lo modularice un toque mas para que sea mas legible

--- main.js
+++ main.js
@@ -13,69 +13,76 @@ import {
 import { extractFormFields } from './comparePdfs/comparePdfs.js';
 import { prefillPdf } from './prefillPdf/prefillPdf.js';
 
+async function mainTransferPdfData() {
+  // Transfer Pdf Data
+  const logsPath = './transferPdfData/resources/outputs/logs.json';
+  const inputFilesDirectory =
+    './transferPdfData/resources/inputs/arrayInputsAndy';
+  const directoryFilePaths = getDirectoryFilePaths(inputFilesDirectory);
+  const templatePath =
+    './transferPdfData/resources/templates/I-765-Template_unlocked.pdf';
+
+  fs.writeFileSync(logsPath, '', { flag: 'w' }); // Restore logs clean
+
+  let successFilesCount = 0;
+  let filesWithErrors = 0;
+  for (const filePath of directoryFilePaths) {
+    const fileName = path.basename(filePath);
+    const fileErrors = await transferPdfData(
+      filePath,
+      templatePath,
+      `./transferPdfData/resources/outputs/arrayOutputsAndy/${fileName}`
+    );
+    logResults(fileName, fileErrors, logsPath);
+
+    fileErrors.failedFieldsCount > 0 ? filesWithErrors++ : successFilesCount++;
+  }
+
+  console.log('\n\nProcess Finished!\n');
+  console.log('Successful Files count:', successFilesCount);
+  console.log('Files with some errors:', filesWithErrors);
+  console.log('For more information please check logs in:', logsPath, '\n');
+}
+
+async function mainCategorizePdfs() {
+  // Categorize pdfs
+  const logsPath = './categorizePdfs/resources/logs.txt';
+  const directoryFilePaths = getDirectoryFilePaths(
+    './categorizePdfs/resources/inputs'
+  );
+  const fileGroups = await categorizePdfs(directoryFilePaths, logsPath);
+  console.log('Files that have the same structure:\n', fileGroups);
+
+  organizeFilesByGroups(fileGroups);
+}
+
+async function mainPrefillPdf() {
+  // Prefill Pdf
+  const prefilledFilePath = './prefillPdf/resources/prefilled.pdf';
+  prefillPdf(
+    './prefillPdf/resources/fieldsArrayDoc.txt',
+    './prefillPdf/resources/new-I-485-2025_01_03-Base.pdf',
+    prefilledFilePath
+  );
+}
+
+async function mainComparePdf() {
+  // Compare Pdf
+  // Reemplazar el 1er y 2do arg por los pdf a comparar
+  const logPath = './comparePdfs';
+  const result = await extractFormFields(
+    './comparePdfs/resources/I-765-Asylum/Current_I-765_Asylum.pdf',
+    './comparePdfs/resources/I-765-Asylum/New_Unlocked_i-765_Asylum.pdf',
+    logPath
+  );
+}
+
 async function main() {
   try {
-    /*
-    // Compare Pdf
-    // Reemplazar el 1er y 2do arg por los pdf a comparar
-    const logPath = './comparePdfs';
-    const result = await extractFormFields(
-      './comparePdfs/resources/I-765-Asylum/Current_I-765_Asylum.pdf',
-      './comparePdfs/resources/I-765-Asylum/New_Unlocked_i-765_Asylum.pdf',
-      logPath
-    );
-    */
-    /*
-    // Prefill Pdf
-    const prefilledFilePath = './prefillPdf/resources/prefilled.pdf';
-    prefillPdf(
-      './prefillPdf/resources/fieldsArrayDoc.txt',
-      './prefillPdf/resources/new-I-485-2025_01_03-Base.pdf',
-      prefilledFilePath
-    );
-    */
-    /*
-    // Transfer Pdf Data
-    const logsPath = './transferPdfData/resources/outputs/logs.json';
-    const inputFilesDirectory =
-      './transferPdfData/resources/inputs/arrayInputsAndy';
-    const directoryFilePaths = getDirectoryFilePaths(inputFilesDirectory);
-    const templatePath =
-      './transferPdfData/resources/templates/I-765-Template_unlocked.pdf';
-
-    fs.writeFileSync(logsPath, '', { flag: 'w' }); // Restore logs clean
-
-    let successFilesCount = 0;
-    let filesWithErrors = 0;
-    for (const filePath of directoryFilePaths) {
-      const fileName = path.basename(filePath);
-      const fileErrors = await transferPdfData(
-        filePath,
-        templatePath,
-        `./transferPdfData/resources/outputs/arrayOutputsAndy/${fileName}`
-      );
-      logResults(fileName, fileErrors, logsPath);
-
-      fileErrors.failedFieldsCount > 0
-        ? filesWithErrors++
-        : successFilesCount++;
-    }
-
-    console.log('\n\nProcess Finished!\n');
-    console.log('Successful Files count:', successFilesCount);
-    console.log('Files with some errors:', filesWithErrors);
-    console.log('For more information please check logs in:', logsPath, '\n');
-    */
-
-    // Categorize pdfs
-    const logsPath = './categorizePdfs/resources/logs.txt';
-    const directoryFilePaths = getDirectoryFilePaths(
-      './categorizePdfs/resources/inputs'
-    );
-    const fileGroups = await categorizePdfs(directoryFilePaths, logsPath);
-    console.log('Files that have the same structure:\n', fileGroups);
-
-    organizeFilesByGroups(fileGroups);
+    // mainComparePdf();
+    // mainPrefillPdf();
+    // mainTransferPdfData();
+    mainCategorizePdfs();
   } catch (err) {
     console.error('Error in main', err);
   }
